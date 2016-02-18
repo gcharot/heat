e8093a1ea850862070e31ca7dbf1e04f4c6ea198
Greg <gcharot@redhat.com>
2016-02-18 11:45:07 +0100

    Added swift url as a user input

--- wordpress/backup_wordpress.sh
+++ wordpress/backup_wordpress.sh
@@ -4,8 +4,15 @@
 #	This script is for demonstration purposes ONLY !!!
 #
 
+#### CHANGE ME ####
+AUTH_URL=http://showcase.rcip.redhat.com:5000/v2.0
+
+
 echo -n  "please enter backup name : "
 read bkp_name
+
+echo -n  "please enter containers folder name : "
+read folder
 
 echo -n "please enter your swift username : "
 read swift_user
@@ -14,7 +21,6 @@ echo -n "please enter your swift password : "
 read -s swift_pwd
 
 ssh db01 "mysqldump -u root -p'mariaroot' wp > $bkp_name"
-echo
 
 if [ $? -eq 0 ]; then
 
@@ -24,11 +30,11 @@ else
 fi
 
 
-ssh db01 "swift --os-auth-url https://ca.enocloud.com:5000/v2.0  --os-username $swift_user --os-password $swift_pwd --os-tenant-name $swift_user upload WP $bkp_name"
+ssh db01 "swift --os-auth-url $AUTH_URL  --os-username $swift_user --os-password $swift_pwd --os-tenant-name $swift_user upload $folder $bkp_name"
 
 if [ $? -eq 0 ]; then
 
-	echo "backup succesfully sent to swift, URL is https://ca.enocloud.com:8080/v1/AUTH_2e9fcfaa8a9f4e0aae8f4404088c8178/WP/$bkp_name"
+	echo "backup succesfully sent to swift, URL is https://showcase.rcip.redhat.com:8080/v1/AUTH_2e9fcfaa8a9f4e0aae8f4404088c8178/WP/$bkp_name"
 else
 	echo "Upload to swift failed"
 fi
